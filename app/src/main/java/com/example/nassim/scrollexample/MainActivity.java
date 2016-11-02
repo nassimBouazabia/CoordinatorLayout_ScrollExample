e846418ac564de29aeb65aef32f3c73edd0eca5e
Nassim <Nassim>
2016-11-02 20:24:53 +0100

    Mise a jour du code source

--- app/src/main/java/com/example/nassim/scrollexample/MainActivity.java
+++ app/src/main/java/com/example/nassim/scrollexample/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.nassim.scrollexample;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     TabLayout tabLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    ImageView imageView;
 
     String[] myList = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21", "Item 22"};
     /**
@@ -38,9 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         initRecyclerView();
         initInstancesDrawer();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
     private void initRecyclerView() {
@@ -52,10 +54,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initInstancesDrawer() {
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(Color.RED);
 
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
